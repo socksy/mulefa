07e95b627260db15a4629f11595cd854019525cc
Ben Lovell <ben.j.lovell@gmail.com>
2016-02-20 15:47:56 +0100

    Stuff for running the graph

--- src/mulefa/core.clj
+++ src/mulefa/core.clj
@@ -1,6 +1,33 @@
 (ns mulefa.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; state checking
+(defn url?
+  [situation string]
+  )
+
+
+;; internal graph running
+(defn- eval-test-with-state
+  [situation [function & args :as test]]
+  (eval (concat (list function situation) args)))
+
+(defn- check-state
+  [state]
+  (map eval-test-with-state state))
+
+(defn- run-transition
+  [state transition])
+
+(defn- run-route
+  [[state transition the-rest]]
+  (run-transition state transition))
+
+(defn- get-routes
+  [graph start-state]
+  ;;TODO
+  (first graph))
+
+(defn run
+  [graph start-state]
+  (->> (get-routes graph start-state)
+       (map run-route)))
