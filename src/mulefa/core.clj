000db168b387949945a4ba55c8ca72b8a7de64a1
Ben Lovell <ben.j.lovell@gmail.com>
2016-06-02 15:54:25 +0200

    Fleshing out some potential methods of achieving stuff in the example... maybe will scrap everything soon

--- src/mulefa/core.clj
+++ src/mulefa/core.clj
@@ -1,19 +1,65 @@
-(ns mulefa.core)
+(ns mulefa.core
+  (:require [clj-webdriver.taxi :as t]))
+
+(defn pass-on
+  "So this turns a function into one that can be called to create a new function
+
+  (kinda like a thunk)
+
+  so:
+  user=> (def foo (pass-on println))
+  #'user/foo
+  user=> (foo \"hello\")
+  #function[user/pass-on/fn-24335/fn-24336]
+  user=> ((foo \"test\"))
+  test
+  nil"
+  [f]
+  (fn [& args]
+    (fn []
+      (apply f args))))
+
+(defn- select
+  [driver selector]
+  (fn []
+    (first (t/find-element driver {:css selector}))))
 
 ;; state checking
 (defn url?
-  [situation string]
-  )
+  [driver string]
+  (fn []
+    (= (t/current-url driver) string)))
 
+
+(def exists? (pass-on t/exists?))
+
+(def value? (pass-on t/visible?))
+
+;; transitions
+(def url (pass-on t/to))
+
+(defn fill-in
+  [driver sel val]
+  (fn []
+    (t/input-text driver (select sel) val)))
+
+(defn submit
+  [driver sel]
+  (fn []
+    (t/submit driver (select sel))))
 
 ;; internal graph running
-(defn- eval-test-with-state
-  [situation [function & args :as test]]
-  (eval (concat (list function situation) args)))
+(defn- eval-with-driver
+  [driver [function & args :as test]]
+  ((concat (list function driver) args)))
 
 (defn- check-state
-  [state]
-  (map eval-test-with-state state))
+  [driver state]
+  (let [_         (println "checking state")
+        new-state (filter #(not= (first %) 'url) state)
+        url-to-go (first (filter #(= (first %) 'url) state))]
+    (when url-to-go (eval-with-driver driver url-to-go))
+    (map (partial eval-with-driver driver) new-state)))
 
 (defn- run-transition
   [driver state transition]
