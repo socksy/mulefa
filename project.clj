68368b4133ff1964839535ea0494d8ab7f0e393c
Ben Lovell <ben.j.lovell@gmail.com>
2016-06-02 15:53:08 +0200

    Adding dependencies to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject mulefa "0.1.0-SNAPSHOT"
-  :description "A declarative testing wrapper for selenium"
+ :description "A declarative testing wrapper for selenium"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-webdriver "0.7.2"]
+                 [org.seleniumhq.selenium/selenium-server "2.47.1"]
+                 [org.seleniumhq.selenium/selenium-java "2.47.0"]
+                 [org.seleniumhq.selenium/selenium-remote-driver "2.47.1"]])
