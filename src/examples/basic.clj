a7c96f2afec8b4e49a473f8fc53c329f083a5908
Joaquim Torres <quim.r.s.torres@gmail.com>
2016-06-20 18:36:50 +0200

    Remove redundant experiment

--- src/examples/basic.clj
+++ src/examples/basic.clj
@@ -1,35 +1,5 @@
 (ns examples.basic
   (:require [mulefa.core :as m]))
-(comment
-;;attempt v0.0.1
-  ; These define the selectors needed
-  (m/def-sel language-picker "#searchLanguage")
-  (m/def-sel search-input "#searchInput")
-  (m/def-sel page-head "#firstHeading")
-  ; or
-  (m/def-sels [language-picker "#searchLanguage"
-               search-input    "#searchInput"
-               page-head       "#firstHeading"])
-
-  (m/def-page home-page   "https://www.wiktionary.org")
-  (m/def-page flocci-page "https://en.wiktionary.org/wiki/floccinaucinihilipilification")
-
-
-  ; a fact is a current state of the world
-  (-> (m/fact home-page
-              [language-picker (m/val "English")
-               search-input    (m/val "floccinaucinihilipilification")])
-
-      ; an act modifies a fact and returns a new fact
-      (m/act (m/submit search-input))
-
-      ; returned fact should have these truth values
-      (m/fact flocci-page
-              [page-head (m/= "floccinaucinihilipilification")
-               page-head (m/!= "noccinaucinihilipilification")]))
-
-  (= returned-fact
-     (m/fact )))
 
 ;; attempt v.0.0.2
 
