757ae6e58605287f0217206e34ecf52d4402eddf
Joaquim Torres <quim.r.s.torres@gmail.com>
2016-06-21 13:05:54 +0200

    Bump selenium dependencies to latest stable versions

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-webdriver "0.7.2"]
-                 [org.seleniumhq.selenium/selenium-server "2.47.1"]
-                 [org.seleniumhq.selenium/selenium-java "2.47.0"]
-                 [org.seleniumhq.selenium/selenium-remote-driver "2.47.1"]])
+                 [org.seleniumhq.selenium/selenium-server "2.49.0"]
+                 [org.seleniumhq.selenium/selenium-java "2.49.0"]
+                 [org.seleniumhq.selenium/selenium-remote-driver "2.49.0"]])
