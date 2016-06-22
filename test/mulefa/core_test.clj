0e15c16d3486b95d4bdf5f4e05a9cebbcb4b172b
Joaquim Torres <quim.r.s.torres@gmail.com>
2016-06-22 14:37:45 +0200

    Copy minimal part of example and create a test out of it

--- test/mulefa/core_test.clj
+++ test/mulefa/core_test.clj
@@ -1,7 +1,26 @@
 (ns mulefa.core-test
   (:require [clojure.test :refer :all]
-            [mulefa.core :refer :all]))
+            [mulefa.core :as m]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def set-up ^:m-transaction
+  [(m/navigate "https://www.wiktionary.org/")])
+
+(def initial ^:m-state
+  #{(m/url?    "https://www.wiktionary.org/")
+    (m/exists? "#searchInput")
+    (m/value?  "#searchLanguage" "en")})
+
+(def search-for-flocci ^:m-transaction
+  [(m/fill-in "#searchInput" "floccinaucinihilipilification")
+   (m/submit "#searchInput")])
+
+(def flocci-state ^:m-state
+  #{(m/exists? "#firstHeading")
+    (m/value?  "#firstHeading" "floccinaucinihilipilification")})
+
+(def graph ^:m-graph
+  #{[set-up initial search-for-flocci flocci-state]})
+
+(deftest test-run
+  (testing "traversal of a minimal graph"
+    (is (nil? (m/run graph set-up)))))
